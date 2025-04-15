539bc553da51119af8d5ea6df8e4ecd675860cd8
wjp <1060638479@qq.com>
2025-04-15 21:48:36 +0800

    feat: update

--- src/main/java/com/wjp/wdada/model/vo/UserVO.java
+++ src/main/java/com/wjp/wdada/model/vo/UserVO.java
@@ -43,5 +43,10 @@ public class UserVO implements Serializable {
      */
     private Date createTime;
 
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
     private static final long serialVersionUID = 1L;
 }
