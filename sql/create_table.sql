c1facf2a4de254ee60e4195050f64225e85e3bb7
wjp <1060638479@qq.com>
2025-04-18 20:30:04 +0800

    update: md

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://wjp.icu">编程导航知识星球</a>
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists wdada;
 
 -- 切换库
-use my_db;
+use wdada;
 
 -- 用户表
 create table if not exists user
